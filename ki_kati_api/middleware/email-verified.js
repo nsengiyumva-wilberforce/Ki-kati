e12201009eec3529be4049970ba70302f09405be
Nsengiyumva Wilberforce <nsengiyumvawilberforce@gmail.com>
2024-10-23 15:28:24 +0300

    separated backend from fontend

--- ki_kati_api/middleware/email-verified.js
+++
@@ -1,9 +0,0 @@
-const checkEmailConfirmed = (req, res, next) => {
-    if (!req.user || !req.user.isEmailConfirmed) {
-      return res.status(403).json({ message: "Email not confirmed. Please confirm your email." });
-    }
-    next();
-  };
-  
-  module.exports = checkEmailConfirmed;
-  
--- ki_kati_api/models/Group.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const GroupSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  members: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-});
-
-module.exports = mongoose.model("Group", GroupSchema);
